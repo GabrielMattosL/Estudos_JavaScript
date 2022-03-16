d32879bf1d7d790afac4262e09d542a3ee45dbe5
Gabriel Antônio <gmattoslopes@gmail.com>
2022-03-15 20:07:08 -0400

    exemplo da aula

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = "Olá, Mundo!";
+const senhaSegura = "123123123";
+const stringNumeros = "34567";
+
+const citacao = 'Meu Nome é: ';
+const meuNome = "Leonardo";
+
+// concatenaçõ (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
+
