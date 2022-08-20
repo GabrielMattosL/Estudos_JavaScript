71453b49896fae9031800867039b32a8b4edae50
Gabriel Antônio <84046030+GabrielMattosL@users.noreply.github.com>
2022-08-20 10:48:05 -0400

    Estudos sobre Objetos

---
+++ Objetos/objetos.js
@@ -0,0 +1,11 @@
+const listaCPFs = ["45234567654", "34565476856", "35845675347"];
+//Array
+//const cliente = ["nome", "André", "idade", 36];
+
+//objeto
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345678901",
+    email:"andre@email.com"
+}
