b9c43f6a23c1f3e6228c151a3ac63f80eade320c
Gabriel <gmattoslopes@gmail.com>
2022-08-21 00:25:09 -0400

    Adicionando e Alterando

---
+++ Objetos/adicionarObjetos.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345678901",
+    email:"andre@email.com"
+}
+
+console.log(cliente)
+
+cliente.email = "(69) 99453-3453"
+
+cliente.telefone = "1234"
+
+console.log(cliente)
