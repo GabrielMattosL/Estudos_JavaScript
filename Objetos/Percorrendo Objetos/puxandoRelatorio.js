bfbb0faba3c3d7ff3bbbb8efa4e6b6d1b4f9916e
Gabriel Antônio <84046030+GabrielMattosL@users.noreply.github.com>
2022-08-29 13:40:24 -0400

    Estudo sobre For-In

---
+++ Objetos/Percorrendo Objetos/puxandoRelatorio.js
@@ -0,0 +1,26 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345678901",
+    email:"andre@email.com",
+    fones:["55912345678", "5534562345", "5534678420"],
+    dependentes:[
+        {
+            nome: "Sara Silva", 
+            parentesco: "filha",
+            dataNasc: "20/03/2011"},
+        {
+            nome: "Erica Silva", 
+            parentesco: "filha", 
+            dataNasc: "14/04/2014"
+        }
+    ]
+}
+
+let relatorio = "";
+
+for ( let info in cliente ) {
+    relatorio += `${info}`
+}
+
+console.log(relatorio)
