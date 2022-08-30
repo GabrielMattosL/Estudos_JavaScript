6e578fb4a81bb1ba5406363ed96c434e6363e46e
Gabriel Antônio de Mattos Lopes <504704@pjro.local>
2022-08-30 09:12:44 -0400

    Metodo Object.keys

---
+++ Objetos/Percorrendo Objetos/ofertaSeguro.js
@@ -0,0 +1,34 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345678901",
+    email:"andre@email.com",
+    fones:["55912345678", "5534562345", "5534678420"],
+    dependentes:[
+        {
+            nome: "Sara Silva", 
+            parentesco: "filha",
+            dataNasc: "20/03/2011"},
+        {
+            nome: "Erica Silva",
+            parentesco: "filha",
+            dataNasc: "14/04/2014"
+        }
+    ],
+
+    saldo: 100,
+
+    depositar: function(valor) {
+        this.saldo += valor
+    }
+}
+
+
+function oferecerSeguro(obj){
+    const propsClientes = Object.keys(obj);
+    if(propsClientes.includes("dependentes")){
+        console.log(`Oferta de seguro de vida para ${obj.nome}`)
+    }
+}
+
+oferecerSeguro(cliente)
