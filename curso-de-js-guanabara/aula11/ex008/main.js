1a38f2b4eb0f624c4de0a06c778c8066684f2237
Gabriel Antônio <84046030+GabrielMattosL@users.noreply.github.com>
2023-08-03 12:57:33 -0400

    Adicionado curso do curso em vídeo

---
+++ curso-de-js-guanabara/aula11/ex008/main.js
@@ -0,0 +1,5 @@
+var vel = 59;
+console.log(`A velocidade do seu carro é ${vel}km/h`);
+if (vel >= 60) {
+    console.log('você foi multado por excesso de velocidade');
+}
---
+++ curso-de-js-guanabara/aula11/ex009/main.js
@@ -0,0 +1,7 @@
+var pais = 'eua'
+
+if (pais == 'brl') {
+    console.log('você é brasileiro');
+} else {
+    console.log('você é extrangeiro');
+}
